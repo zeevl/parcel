97298a2cacf85f555f68c1eca0d4832c3b9b2905
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-06-16 12:48:38 +0200

    Coffeescript sourcemaps

--- packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
+++ packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
@@ -1,11 +1,26 @@
+// @flow
+
 import {Transformer} from '@parcel/plugin';
+import SourceMap from '@parcel/source-map';
 import coffee from 'coffeescript';
 
 export default new Transformer({
-  async transform({asset}) {
+  async transform({asset, options}) {
     asset.type = 'js';
-    let output = coffee.compile(await asset.getCode());
-    asset.setCode(output);
+
+    if (options.sourceMaps) {
+      let {js, v3SourceMap} = coffee.compile(await asset.getCode(), {
+        sourceMap: true,
+        bare: true
+      });
+
+      asset.setCode(js);
+      asset.setMap(SourceMap.fromRawSourceMap(v3SourceMap));
+    } else {
+      let js = coffee.compile(await asset.getCode(), {bare: true});
+      asset.setCode(js);
+    }
+
     return [asset];
   }
 });
